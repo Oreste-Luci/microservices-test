3c7fdddac6182235a4f2ea95560d3f35a51fbca0
oluci-ob <oluci@oneboxtm.com>
2015-04-27 11:15:27 +0200

    Configuration changes

--- ms_b/src/main/java/test/microservices/b/MSTestBConfiguration.java
+++ ms_b/src/main/java/test/microservices/b/MSTestBConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+/**
+ * @author Oreste Luci
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MSTestBConfiguration {
--- ms_eureka/src/main/java/test/microservices/eureka/EurekaServerApp.java
+++ ms_eureka/src/main/java/test/microservices/eureka/EurekaServerApp.java
@@ -1,18 +1,16 @@
 package test.microservices.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
- * By Oreste Luci
+ * @author Oreste Luci
  */
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServerApp {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApp.class, args);
